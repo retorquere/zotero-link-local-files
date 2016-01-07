91e36f7819d96d042666311e7f0aded3c9ebd5cf
retorquere <emiliano.heyns@iris-advies.com>
2016-01-07 12:01:59 +0100

    travis releases

--- .travis.sh
+++ .travis.sh
@@ -3,30 +3,40 @@
 set -e
 set -u
 
-rake
-XPI=`ls *.xpi`
+DEBUGBUILD=false XPI=`rake xpi`
+
 RELEASE="$TRAVIS_COMMIT release: $XPI"
 CHECKIN=`git log -n 1 --pretty=oneline`
 echo "checkin: $CHECKIN"
 echo "release: $RELEASE"
 if [ "$CHECKIN" = "$RELEASE" ] ; then
   STATUS=`travis_parallel_sentinel script`
-  if [ "$STATUS" = "deploy" ] ; then
-    sed -i.bak -e 's/git@github.com:/https:\/\/github.com\//' .gitmodules
-    if [ -f .git/modules/www/config ] ; then # how can this be absent?!
-      sed -i.bak -e 's/git@github.com:/https:\/\/github.com\//' .git/modules/www/config
-    fi
-    echo "https://${GITHUB_TOKEN}:@github.com" > /tmp/credentials
-    git config --global credential.helper "store --file=/tmp/credentials"
-
-    git submodule update --init
-    (cd www && git checkout master && git pull)
-    git config --global user.name "retorquere"
-    git config --global user.email "retorquere@ZotPlus.github.com"
-    git config --global push.default matching
-    bundle exec rake deploy
-    git checkout . # prevent travis release rebuilds?
+  if [ "$STATUS" != "deploy" ] ; then
+    exit
   fi
+
+  export CHANGELOG_GITHUB_TOKEN="$GITHUB_TOKEN"
+  rm -f *.xpi
+  DEBUGBUILD=false rake
+
+  sed -i.bak -e 's/git@github.com:/https:\/\/github.com\//' .gitmodules
+  if [ -f .git/modules/www/config ] ; then # how can this be absent?!
+    sed -i.bak -e 's/git@github.com:/https:\/\/github.com\//' .git/modules/www/config
+  fi
+  echo "https://${GITHUB_TOKEN}:@github.com" > /tmp/credentials
+  git config --global credential.helper "store --file=/tmp/credentials"
+
+  git submodule update --init
+  (cd www && git checkout master && git pull)
+  git config --global user.name "retorquere"
+  git config --global user.email "retorquere@ZotPlus.github.com"
+  git config --global push.default matching
+  bundle exec rake deploy
 else
-  echo 'not a tagged release'
+  STATUS=`travis_parallel_sentinel script || true`
+  if [ "$STATUS" != "deploy" ] ; then
+    exit
+  fi
+
+  ./bin/xpi-to-s3
 fi
